de8fcec76639673c66098cf18a42eba01eb338d8
dvl727 <jpm727@att.net>
2015-02-17 19:47:23 -0600

    Additional Comments

--- cachematrix.R
+++ cachematrix.R
@@ -20,14 +20,17 @@ list(set=set, get=get,
 #### CacheSolved function computes the inverse of a provided matrix, x.
 ### the function checks if 'm' is already computed, it returns it from 
 ### Cache. Otherwise, it recomputes the inverse and sets the value to m
+
 cacheSolve <- function(x=matrix(), ...) {
-    m<-x$getmatrix()
-    if(!is.null(m)){
-      message("getting cached data")
-      return(m)
+    m<-x$getmatrix()          "get m from x (returned from makeCacheMatrix"
+    if(!is.null(m)){       "m is not null only when it has been calc before"
+      message("getting cached inverse matrix") 
+      return(m)                           "Return the mean"
     }
-    datos<-x$get()
-    m<-solve(datos, ...)
-    x$setmatrix(m)
+    
+    ## Below code is only called when m is null 
+    datos<-x$get()        "Gets the matrix made in makeCacheMatrix"
+    m<-solve(datos, ...)  "calculates inverse of the matrix"
+    x$setmatrix(m)        "Stores the inverse "
     m
 }
